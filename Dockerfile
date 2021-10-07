4ca027c3564bd94eb1a3e153ce57c8e0c1c2833d
sjanagill <sonika.janagill@gmail.com>
2021-10-07 07:28:13 +0100

    Updated EC2 instance to use Elastic IPs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:13-jdk-alpine
 VOLUME /tmp
 COPY target/*.jar peoplesplace-cafe-0.0.1-SNAPSHOT.jar
+ENV PORT 7575
 EXPOSE 7575/tcp
 ENTRYPOINT ["java","-jar","/peoplesplace-cafe-0.0.1-SNAPSHOT.jar"]
--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ echo "Generating jar file"
 
 #Copy execute_commands_on_ec2.sh file which has commands to be executed on server... Here we are copying this file
 # every time to automate this process through 'deploy.sh' so that whenever that file changes, it's taken care of
-scp -i "C:/Users/sjanagill/Documents/PP_Demo.pem" execute_commands_on_ec2.sh ec2-user@ec2-35-177-63-156.eu-west-2.compute.amazonaws.com:/home/ec2-user
+scp -i "C:/Users/sjanagill/Documents/PP_Demo.pem" execute_commands_on_ec2.sh ec2-user@ec2-18-169-2-189.eu-west-2.compute.amazonaws.com:/home/ec2-user
 echo "Copied latest 'execute_commands_on_ec2.sh' file from local machine to ec2 instance"
 
-scp -i "C:/Users/sjanagill/Documents/PP_Demo.pem" target/peoplesplace-cafe-0.0.1-SNAPSHOT.jar ec2-user@ec2-35-177-63-156.eu-west-2.compute.amazonaws.com:/home/ec2-user
+scp -i "C:/Users/sjanagill/Documents/PP_Demo.pem" target/peoplesplace-cafe-0.0.1-SNAPSHOT.jar ec2-user@ec2-18-169-2-189.eu-west-2.compute.amazonaws.com:/home/ec2-user
 echo "Copied jar file from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
-ssh -i "C:/Users/sjanagill/Documents/PP_Demo.pem" ec2-user@ec2-35-177-63-156.eu-west-2.compute.amazonaws.com ./execute_commands_on_ec2.sh
+ssh -i "C:/Users/sjanagill/Documents/PP_Demo.pem" ec2-user@ec2-18-169-2-189.eu-west-2.compute.amazonaws.com ./execute_commands_on_ec2.sh
